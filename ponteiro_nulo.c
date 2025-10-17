6568e04a7d602ee6f343973c015ab2821c1d753b
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-17 13:06:35 -0300

    .

---
+++ ponteiro_nulo.c
@@ -0,0 +1,13 @@
+// Crie um ponteiro nulo (NULL) e mostre uma mensagem de erro se tentar acessá-lo.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("clear");
+    int *ptr = NULL;
+
+    printf("\n\n%d\n\n", *ptr);
+
+    return 0;
+}
+ 
