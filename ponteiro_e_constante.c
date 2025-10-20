a555a0aac560dd9ef2250de63f73d238c9f6f6aa
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-20 09:58:48 -0300

    .

---
+++ ponteiro_e_constante.c
@@ -0,0 +1,13 @@
+// Crie um ponteiro para constante e tente modificar o valor (observe o erro).
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("clear");
+    int n1 = 1, n2 = 2;
+    const int *num = &n1;
+
+    *num = n2;
+
+    return 0;
+}
